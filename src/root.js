9358896c8c823b7131f0f150a22a0707ac95019c
Peter Johanson <peter@peterjohanson.com>
2015-01-26 23:22:00 -0500

    Handle extensions with no initialize function.

--- src/root.js
+++ src/root.js
@@ -4,7 +4,13 @@ var _ = require('lodash');
 var WebLink = require('./web_link');
 
 var Root = function(url, http, extensions) {
-  _.forEach(extensions, function(e) { e.initialize(http, extensions); });
+  _.forEach(extensions, function(e) {
+    if (!e.initialize) {
+      return;
+    }
+
+    e.initialize(http, extensions);
+  });
   var link = new WebLink({ href: url }, http, extensions);
 
   this.follow = function(options) {
