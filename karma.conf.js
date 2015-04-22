1d74f9492a339a2d30113a19ca9c421ccb2a0a9d
Peter Johanson <peter@peterjohanson.com>
2015-04-22 09:36:12 -0400

    Disable PhantomJS for karma tests (for now).
    * Something in our tests is causing PhantomJS to die painfully
      and quietly... Disable it for now and use FF for Karma tests.

--- karma.conf.js
+++ karma.conf.js
@@ -60,7 +60,8 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS', 'Firefox'],
+    browsers: ['Firefox'],
+    //browsers: ['PhantomJS', 'Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
