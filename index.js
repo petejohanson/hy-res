5f443cf477ca6b9e1603ec372599db80eead2e38
Peter Johanson <peter@peterjohanson.com>
2015-04-30 14:31:46 -0400

    Update 2015-04-30T18:31:43.675Z

--- index.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  Root: require('./src/root'),
-  Form: require('./src/form'),
-  HalExtension: require('./src/hal'),
-  JsonExtension: require('./src/json'),
-  LinkHeaderExtension: require('./src/link_header'),
-  SirenExtension: require('./src/siren'),
-};
