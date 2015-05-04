f3e2a7bd21c61a382f874f9222fa086da909a34d
Peter Johanson <peter@peterjohanson.com>
2015-05-04 14:19:31 -0400

    Test both FF and PhantomJS.

--- karma.conf.js
+++ karma.conf.js
@@ -74,7 +74,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['PhantomJS', 'Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
